7bfd5e96229cb0b09615563903befbe231487a1d
sudhir123 <madaan.sudhir@gmail.com>
2024-09-01 13:21:28 +0530

    security group error2

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_groups = [aws_security_group.blog.id] 
+  vpc_security_group_ids = [aws_security_group.blog.id] 
 
   tags = {
     Name = "Learning Terraform"
